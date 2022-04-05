0d02bdae099f48f32b638f767a8093976c1e126b
Zaen67 <96893984+Zaen67@users.noreply.github.com>
2022-04-05 21:31:40 +0700

    Update db.py

--- Music/Plugins/db.py
+++ Music/Plugins/db.py
@@ -1,20 +1,23 @@
-from Music import app, SUDOERS
-from pyrogram import filters, Client
-from pyrogram.types import Message
 import asyncio
-from Music.MusicUtilities.database.chats import (get_served_chats, is_served_chat, add_served_chat, get_served_chats)
-from Music.MusicUtilities.helpers.filters import command
+
+from pyrogram.types import Message
+from pyrogram import filters, Client
+
+from Music import app, OWNER
+from ..MusicUtilities.helpers.filters import command
+from Music.MusicUtilities.database.chats import get_served_chats
 
 
-@app.on_message(filters.command("broadcast") & filters.user(SUDOERS))
-async def broadcast_message(_, message):
+@app.on_message(command(["broadcast_pin"]) & filters.user(OWNER))
+async def broadcast_message_pin(_, message):
     if not message.reply_to_message:
         pass
-    else :
+    else:
+        msg = await message.reply_text("🔄 Broadcasting message...")
         x = message.reply_to_message.message_id   
         y = message.chat.id
         sent = 0
-        pin = 0
+        pins = 0
         chats = []
         schats = await get_served_chats()
         for chat in schats:
@@ -24,21 +27,22 @@ async def broadcast_message(_, message):
                 m = await app.forward_messages(i, y, x)
                 try:
                     await m.pin(disable_notification=False)
-                    pin += 1
+                    pins += 1
                 except Exception:
                     pass
                 await asyncio.sleep(.3)
                 sent += 1
             except Exception:
                 pass
-        await message.reply_text(f"**Broadcasted Message In {sent}  Chats with {pin} Pins.**")  
+        await msg.edit_text(f"✅ Broadcasted message in {sent} chats\n📌 Sent with {pins} chat pins.")  
         return
     if len(message.command) < 2:
-        await message.reply_text("**Usage**:\n/broadcast [MESSAGE]")
-        return  
+        await message.reply_text("**usage**:\n\n/broadcast_pin (message)")
+        return
+    msg = await message.reply_text("🔄 Broadcasting message...")
     text = message.text.split(None, 1)[1]
     sent = 0
-    pin = 0
+    pins = 0
     chats = []
     schats = await get_served_chats()
     for chat in schats:
@@ -48,11 +52,55 @@ async def broadcast_message(_, message):
             m = await app.send_message(i, text=text)
             try:
                 await m.pin(disable_notification=False)
-                pin += 1
+                pins += 1
             except Exception:
                 pass
             await asyncio.sleep(.3)
             sent += 1
         except Exception:
             pass
-    await message.reply_text(f"**Broadcasted Message In {sent} Chats and {pin} Pins.**")
+    await msg.edit_text(f"✅ Broadcasted message in {sent} chats\n📌 Sent with {pins} chat pins.")
+
+
+@app.on_message(command(["broadcast"]) & filters.user(OWNER))
+async def broadcast_message_nopin(_, message):
+    if not message.reply_to_message:
+        pass
+    else:
+        msg = await message.reply_text("🔄 Broadcasting message...")
+        x = message.reply_to_message.message_id
+        y = message.chat.id
+        sent = 0
+        chats = []
+        schats = await get_served_chats()
+        for chat in schats:
+            chats.append(int(chat["chat_id"]))
+        for i in chats:
+            try:
+                m = await app.forward_messages(i, y, x)
+                await asyncio.sleep(0.3)
+                sent += 1
+            except Exception:
+                pass
+        await msg.edit_text(f"✅ Broadcasted message in {sent} chats")
+        return
+    if len(message.command) < 2:
+        await message.reply_text(
+            "**usage**:\n\n/broadcast (message)"
+        )
+        return
+    msg = await message.reply_text("🔄 Broadcasting message...")
+    text = message.text.split(None, 1)[1]
+    sent = 0
+    chats = []
+    schats = await get_served_chats()
+    for chat in schats:
+        chats.append(int(chat["chat_id"]))
+    for i in chats:
+        try:
+            m = await app.send_message(i, text=text)
+            await asyncio.sleep(0.3)
+            sent += 1
+        except Exception:
+            pass
+    await msg.edit_text(f"✅ Broadcasted message in {sent} chats")
