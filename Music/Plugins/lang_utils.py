4cc29eeaf322356e8bf6042bba72f621a89bfd13
Zaen67 <96893984+Zaen67@users.noreply.github.com>
2022-04-07 10:47:41 +0700

    Update lang_utils.py

--- Music/Plugins/lang_utils.py
+++ Music/Plugins/lang_utils.py
@@ -3,7 +3,7 @@ from os import path, listdir
 
 from pyrogram import emoji
 
-from Music_database import filters
+from Music.MusicUtilities.database import filters
 
 lang_folder = path.join(path.dirname(path.realpath(__file__)), "lang")
 code = ""
